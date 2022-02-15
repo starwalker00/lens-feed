6ea28be748d4067bc30c6aff1a96949c28dacd8f
starwalker00 <aaronwalkerup@gmail.com>
2022-02-15 18:27:00 +0100

    initial setup done

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  styles: {
+    global: {
+      'html, body': {
+        color: '#00501e',
+        background: '#e5ffbd',
+      },
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
-
 export default MyApp
