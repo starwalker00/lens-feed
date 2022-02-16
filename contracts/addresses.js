44d813d4bc4bc12d16f54b037116ef019a1809fa
starwalker00 <aaronwalkerup@gmail.com>
2022-02-16 23:52:02 +0100

    add contracts

---
+++ contracts/addresses.js
@@ -0,0 +1,7 @@
+const addresses = {
+    lensHubProxy: "0xF6BF84E5df229029C9D36dC7ABaCDBE9c0bd7b4F",
+    emptyCollectModule: "0xEDeB556d916d9e6E154A083Ac18Da52cc5eD8b1c",
+    ZERO_ADDRESS: "0x0000000000000000000000000000000000000000"
+};
+
+export default addresses;
