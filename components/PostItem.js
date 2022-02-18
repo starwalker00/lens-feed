f1e491fe94a9619e448dd48e9d8228741a3c1b5c
starwalker00 <aaronwalkerup@gmail.com>
2022-02-18 19:46:41 +0100

    move URI handler to helper file

--- components/PostItem.js
+++ components/PostItem.js
@@ -1,8 +1,6 @@
 import { Box, Center, Flex, Image, Text, WrapItem, VStack, HStack, Spacer, Heading, Tooltip } from '@chakra-ui/react'
-import parseDataURL from 'data-urls'
-import { labelToName, decode } from 'whatwg-encoding'
-import DOMPurify from 'isomorphic-dompurify'
 import PostItemTitle from './PostItemTitle'
+import { resolveContentURI } from '../lib/URIhelper.js'
 
 function PostItem({ postData }) {
     // console.log(`postData: ${JSON.stringify(postData)}`)
@@ -23,52 +21,6 @@ function PostItem({ postData }) {
     // const contentURI = "https://ipfs.io/ipfs/bafybeie53rvcgggxflkqdmcpffsldpy62jfqrs22ryfyd3c3jvznou7zee"
     // const contentURI = "https://picsum.photos/500/200"
 
-    function resolveContentURI(contentURI) {
-        const breakingReturnValue = {
-            mimeType: 'text/plain',
-            bodyDecoded: 'Error : No compatible content to show'
-        }
-        try { // try text/plain decoding
-            // console.log('text/plain decoding')
-            const dataURL = parseDataURL(contentURI)
-            const mimeType = dataURL.mimeType.toString()
-            if (mimeType.startsWith('text/plain')) {
-                const encodingName = labelToName(dataURL.mimeType.parameters.get("charset") || "utf-8")
-                const bodyDecoded = decode(dataURL.body, encodingName)
-                return { mimeType: mimeType, bodyDecoded: bodyDecoded }
-            }
-        }
-        catch (error) {
-            // console.log('could not parse string')
-            // console.log(error)
-        }
-        try { // try text/html decoding
-            // console.log('text/html decoding')
-            let dataURL = parseDataURL(contentURI)
-            let mimeType = dataURL.mimeType.toString()
-            if (mimeType === 'text/html') {
-                const encodingName = labelToName(dataURL.mimeType.parameters.get("charset") || "utf-8")
-                const bodyDecoded = decode(dataURL.body, encodingName)
-                return { mimeType: mimeType, bodyDecoded: DOMPurify.sanitize(bodyDecoded) }
-            }
-        }
-        catch (error) {
-            // console.log('could not parse string')
-            // console.log(error)
-        }
-        try { // try image as link (frequent use case)
-            // console.log('mage as link decoding')
-            if (contentURI.toLowerCase().startsWith('https://')) {
-                return { mimeType: 'image', bodyDecoded: contentURI }
-            }
-        }
-        catch (error) {
-            // console.log('could not parse string')
-            // console.log(error)
-        }
-        //if nothing matched :
-        return breakingReturnValue
-    }
     const resolvedURI = resolveContentURI(contentURI)
     const mimeType = resolvedURI.mimeType
     const bodyDecoded = resolvedURI.bodyDecoded
