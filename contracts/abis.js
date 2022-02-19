581f793f975151681409b51af6509f1f8601bcde
starwalker00 <aaronwalkerup@gmail.com>
2022-02-19 12:39:05 +0100

    add mockprofilemumbai contract add & abi

--- contracts/abis.js
+++ contracts/abis.js
@@ -1,7 +1,9 @@
 import lensHubProxyAbi from "./abis/lensHubProxy.json";
+import MockProfileCreationProxyAbi from "./abis/MockProfileCreationProxy.json";
 
 const abis = {
-    lensHubProxy: lensHubProxyAbi
+    lensHubProxy: lensHubProxyAbi,
+    MockProfileCreationProxy: MockProfileCreationProxyAbi
 };
 
 export default abis;
