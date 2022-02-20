3566b006f9565de48387b716f8adc1e4aedd3639
starwalker00 <aaronwalkerup@gmail.com>
2022-02-20 14:49:42 +0100

    improve loading profile page

--- pages/profile/[profileHandle].js
+++ pages/profile/[profileHandle].js
@@ -8,54 +8,71 @@ import {
 
 import Head from 'next/head'
 import { Container, Box, Heading, Text, Link, Flex } from '@chakra-ui/react'
+import { Spinner } from '@chakra-ui/react'
 import ProfilePagePostList from '../../components/ProfilePagePostList'
 import PostCreator from '../../components/PostCreator'
 import TimeAgo from 'javascript-time-ago'
 import en from 'javascript-time-ago/locale/en.json'
 TimeAgo.addDefaultLocale(en)
 
-export default function Profile({ results }) {
-    console.log(results)
+export default function Profile({ results, profileHandle }) {
+    // console.log(results?.posts?.length)
+    // console.log(profileHandle)
     const { isFallback } = useRouter(); // waiting state
     return (
-        <main>
-            {isFallback ?
-                <p>Fallback</p>
-                :
-                <Container maxWidth='container.sm' bg='white' p='0' overflow='hidden'>
-                    <Head>
-                        <title>Lens feed</title>
-                        <meta name="description" content="Lens Protocol global feed with post feature" />
-                        <link rel="icon" href="/flower.svg" />
-                    </Head>
-                    <Box w='100%' m="20px auto" textAlign="center">
-                        <Heading as="h1" size="xl">
-                            Lens feed
-                        </Heading>
-                        <Text fontSize='lg' mt="10px" letterSpacing='.12rem'>
-                            global feed of posts on Lens Protocol
-                        </Text>
-                    </Box>
-                    {/* <Box w='100%' m="20px auto" textAlign="center">
+        <Container maxWidth='container.sm' bg='white' p='0' overflow='hidden'>
+            <Head>
+                <title>Lens feed</title>
+                <meta name="description" content="Lens Protocol global feed with post feature" />
+                <link rel="icon" href="/flower.svg" />
+            </Head>
+            <Box w='100%' m="20px auto" textAlign="center">
+                <Heading as="h1" size="xl">
+                    Lens feed
+                </Heading>
+                <Text fontSize='lg' mt="10px" letterSpacing='.12rem'>
+                    global feed of posts on Lens Protocol
+                </Text>
+            </Box>
+            {/* <Box w='100%' m="20px auto" textAlign="center">
                         <PostCreator />
                     </Box> */}
-                    {/* {console.log(`fallbackData : ${JSON.stringify(fallbackData)}`)} */}
-                    <Box w='100%' m="20px auto" textAlign="center">
-                        <Heading as="h2" size="lg">
-                            Latests Posts
-                        </Heading>
-                    </Box>
-                    <ProfilePagePostList results={results} />
-                    <Flex alignItems='center'>
-                        <Flex>
-                            {isFallback
-                                ? 'This is statically generating.'
-                                : 'This was statically generated.'}
-                        </Flex>
+            {/* {console.log(`fallbackData : ${JSON.stringify(fallbackData)}`)} */}
+            <Box w='100%' m="20px auto" textAlign="center">
+                <Heading as="h2" size="lg">
+                    Posts of {profileHandle}
+                </Heading>
+            </Box>
+            {isFallback // page is laoding
+                ?
+                <Flex p='4' direction='column' alignItems='center'>
+                    <Flex p='4'>
+                        <Spinner size='xl' />
                     </Flex>
-                </Container>
+                    <Flex p='4'>
+                        <Text s="h1" size="md">
+                            Loading posts...
+                        </Text>
+                    </Flex>
+                </Flex>
+                :
+                results
+                    ?
+                    <ProfilePagePostList results={results} />
+                    : <Flex>
+                        <Heading s="h1" size="md">
+                            No posts found
+                        </Heading>
+                    </Flex>
             }
-        </main>
+            {/* <Flex mt='10' alignItems='center'>
+                <Flex>
+                    {isFallback
+                        ? 'This is statically generating.'
+                        : 'This was statically generated.'}
+                </Flex>
+            </Flex> */}
+        </Container>
     )
 }
 
@@ -73,25 +90,10 @@ export async function getStaticProps({ params }) {
         const ID = profileID.profiles[0].id
         console.log(ID)
         const results = await getPostsByProfileID(ID);
-        console.log(results)
-        return { props: { results } };
+        // console.log(results)
+        return { props: { profileHandle, results } };
     } catch (error) {
         console.error(error);
         return { props: { profileHandle, notFound: true } };
     }
-
-
-    return { props: { profileHandle, notFound: false } };
-    // if (tweet.length > 40 || !TWEET_ID.test(tweet)) {
-    //     return { notFound: true };
-    // }
-
-    // try {
-    //     const ast = await fetchTweetAst(tweet);
-    //     return ast ? { props: { ast } } : { notFound: true };
-    // } catch (error) {
-    //     // The Twitter API most likely died
-    //     console.error(error);
-    //     return { notFound: true };
-    // }
 }
