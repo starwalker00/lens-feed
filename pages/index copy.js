cae62eef32b8a114da0741a23c51f8ab405f814c
starwalker00 <aaronwalkerup@gmail.com>
2022-02-25 14:58:53 +0100

    add pause message

--- pages/index copy.js
+++ pages/index copy.js
@@ -27,52 +27,16 @@ export default function Home({ fallbackData }) {
           global feed of posts on Lens Protocol
         </Text>
       </Box>
-      {/* <Box w='100%' m="20px auto" textAlign="center">
+      <Box w='100%' m="20px auto" textAlign="center">
         <PostCreator />
-      </Box> */}
+      </Box>
       {/* {console.log(`fallbackData : ${JSON.stringify(fallbackData)}`)} */}
-      {/* <Box w='100%' m="20px auto" textAlign="center">
+      <Box w='100%' m="20px auto" textAlign="center">
         <Heading as="h2" size="lg">
           Latests Posts
         </Heading>
-      </Box> */}
-      {/* <PostList results={fallbackData} /> */}
-      <Box w='100%' m="100px auto" textAlign="center">
-        <Heading as="h1" size="xl">
-          Experiment paused.
-        </Heading>
-        <Text fontSize='xl' mt="20px" letterSpacing='.12rem'>
-          A user with presumably bad intentions is using a bot to spam this website.
-        </Text>
-
-        <Text fontSize='xl' mt="20px" letterSpacing='.12rem'>
-          Thank you to everyone who has used this interface to interact with the Lens Protocol.
-          We will be back with more features very soon.
-        </Text>
-        <Text fontSize='xl' mt="10px" letterSpacing='.12rem'>
-          Keep experimenting.
-        </Text>
-        <Text fontSize='lg' mt="100px" letterSpacing='.12rem'>
-          Here are some things to check out why we build a brighter future.
-          <br />
-          <Link color='teal.500' href='https://twitter.com/Ukraine_DAO'>
-            https://twitter.com/Ukraine_DAO
-          </Link>
-          <br />
-          <Link color='teal.500' href='https://aika.market/'>
-            https://aika.market/
-          </Link>
-          <br />
-          <Link color='teal.500' href='https://twitter.com/wearenewt'>
-            https://twitter.com/wearenewt
-          </Link>
-        </Text>
       </Box>
-      <Box w='100%' m="100px auto" textAlign="center">
-        <Text fontSize='sm' mt="20px" letterSpacing='.12rem'>
-          Independently built by szutok.
-        </Text>
-      </Box>
+      <PostList results={fallbackData} />
     </Container>
   )
 }
